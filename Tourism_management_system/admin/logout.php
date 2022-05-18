5637c7276d815b84ac0e4ae7d4052e47bd80a607
Md. Rahat Hasan <57935394+rahat7872@users.noreply.github.com>
2022-05-18 14:26:49 +0600

    Delete Tourism_management_system directory

--- Tourism_management_system/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require('config.php');
-session_destroy();
-unset($_SESSION['username']);
-$_SESSION['message']="You are now logged out";
-header("location: ../admin.php");
-?>
